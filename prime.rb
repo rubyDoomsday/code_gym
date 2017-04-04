7a8803f1f4ea516c1a8c5bdf16ab965bf3a36820
Rebecca Chapin <rebecca.chapin@validic.com>
2017-04-04 12:54:50 -0400

    prime

--- prime.rb
+++ prime.rb
@@ -1,11 +1,14 @@
+# Finds prime numbers
 class Prime
   attr_accessor :numbers, :prime_numbers
 
-  def initialize(find = default_stop)
-    @total = find
+  # @param total [Integer] the number of primes you want to find
+  def initialize(total)
+    @total = total || default_stop
     @prime_numbers = []
   end
 
+  # Builds an array of prime numbers
   def find_prime
     x = 2
     while @prime_numbers.count < @total
@@ -14,24 +17,29 @@ class Prime
     end
   end
 
+  # Shows the array of prime numbers as a string
   def show_prime
     @prime_numbers.uniq.to_s
   end
 
   private
 
-  def prime?(x)
-    (2...x).each do |y|
-      return false if (x % y).zero?
+  # Determines if the provided number is prime
+  # @param number [Integer] number to check
+  def prime?(number)
+    (2...number).each do |x|
+      return false if (number % x).zero?
     end
     true
   end
 
+  # Defualt stopping point if none provided
   def default_stop
     @total ||= 1_000
   end
 end
 
-report = Prime.new
+this_many = ARGV[0].to_i unless ARGV[0].nil?
+report = Prime.new(this_many)
 report.find_prime
 puts report.show_prime
